703d0435409e615ea40527389d57646fc0aec59f
Andrew Strader <astrader@resourceammirati.com>
2018-03-20 14:23:28 -0400

    define start task with browser-sync etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,44 @@
 const gulp = require('gulp');
+const rimraf = require('rimraf');
+const runSequence = require('run-sequence');
+const browserSync = require('browser-sync');
 
 const HandlebarsGenerator = require('/Users/astrader/ProjectCode/node/handlebars-generator');
 
-gulp.task('build', ['build:views'], () => {
-
+gulp.task('clean', [], (cb) => {
+	rimraf('dist/**/*', cb);
 });
 
-gulp.task('build:views', [], ()=>{
+gulp.task('build', [], (cb) => {
+	runSequence('clean', 'build:all', cb);
+});
+
+gulp.task('watch', [], (cb) => {
+	runSequence('clean', 'watch:all', cb);
+});
+
+gulp.task('serve', ['watch'], (cb) => {
+	var bs = browserSync.create();
+	bs.init({
+		files: 'dist/**/*',
+		server: {
+			baseDir: 'dist'
+		}
+	}, cb);
+});
+
+gulp.task('watch:all', ['watch:views']);
+
+gulp.task('build:all', ['build:views']);
+
+// VIEWS
+
+gulp.task('build:views', [], () => {
 	return HandlebarsGenerator.generateSite('views', 'dist', {
 		sourceExtension: 'hbs',
 	});
+});
+
+gulp.task('watch:views', ['build:views'], () => {
+	gulp.watch(['views/**/*.+(hbs|js)'], ['build:views']);
 });

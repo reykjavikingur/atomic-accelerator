382760989bcf4bc8d7e7969b2e8ed14454cad718
Andrew Strader <astrader@resourceammirati.com>
2018-03-26 19:58:48 -0400

    never mind about autocompleting search form for now

--- src/scripts/lib/views/search-form.js
+++ src/scripts/lib/views/search-form.js
@@ -1,7 +1,7 @@
 module.exports = `
+<!-- search-form.js -->
 <form method="get" action="<%= this.url.pathname %>">
     <input type="text" name="q" value="<%= this.url.query.q %>" />
-    <!-- TODO autocomplete the list of names -->
     <input type="submit" value="Search" />
     <a href="<%= this.url.pathname %>">full list</a>
 </form>
